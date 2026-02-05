d84bac72901cc5ed01e98e9dbdada7199b78e673
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2026-02-05 11:47:55 -0500

    Update Site

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-window.config = {
+const config = {
   "property": {
     "streetAddress": "3 Ambleside Ave",
     "city": "Toronto",
@@ -9,14 +9,14 @@ window.config = {
   "realtor": {
     "fullName": "Stephen Fabbro",
     "title": "REALTOR®",
-    "phone": "6479229302",
+    "phone": "(647) 922-9302",
     "email": "stephen@theskygroup.ca",
     "photoUrl": "https://salesgenius.s3.ca-central-1.amazonaws.com/uploads/1761147528200-Headshot - Stephen Fabbro (1).png",
     "bio": "With over 15 years of combined experience, Shar, Oudi, and Stephen bring a personalized, strategic approach to Toronto real estate. As a close-knit team, they specialize in helping buyers, sellers, and investors navigate the market with clarity, confidence, and ease. Their process is built on trust, transparency, and tailored strategies that align with your unique goals—whether you're buying your first home, upgrading, or making a smart investment. With a deep understanding of market trends and a hands-on approach, Shar, Oudi, and Stephen are committed to delivering results while keeping the process smooth and stress-free",
     "social": {
       "facebook": "https://www.facebook.com/theskygroupre",
       "instagram": "https://www.instagram.com/theskygroup_re",
-      "website": "#"
+      "website": "https://theskygroup.ca/"
     }
   },
   "brokerage": {
@@ -27,35 +27,31 @@ window.config = {
   "openHouse": {
     "eventDate": "",
     "bundleItems": [
-            {
-                  "icon": "fas fa-home",
-                  "text": "Full Property Listing",
-                  "url": "https://www.realtor.ca/real-estate/29321202/3-ambleside-avenue-toronto-stonegate-queensway-stonegate-queensway?propertyId=29321202&"
-            },
-            {
-                  "icon": "fas fa-vr-cardboard",
-                  "text": "3D Virtual Tour",
-                  "url": "https://tenzi-homes.aryeo.com/sites/3-ambleside-ave-toronto-on-m8z-2h5-20240416/branded"
-            },
-            {
-                  "icon": "fas fa-ruler-combined",
-                  "text": "Architectural Floor Plan",
-                  "url": "https://tenzi-homes.aryeo.com/listings/019a3324-43b0-7168-8f9d-d61c4e177ee0/interactive-floor-plans/editor"
-            },
-            {
-                  "icon": "fas fa-video",
-                  "text": "Video Tour",
-                  "url": "https://tenzi-homes.aryeo.com/videos/019a5c46-7f05-70c3-b099-65fe74c44886?v=187"
-            }
-      ]
+      {
+        "icon": "fas fa-home",
+        "text": "Full Property Listing"
+      },
+      {
+        "icon": "fas fa-vr-cardboard",
+        "text": "3D Virtual Tour"
+      },
+      {
+        "icon": "fas fa-ruler-combined",
+        "text": "Architectural Floor Plan"
+      },
+      {
+        "icon": "fas fa-video",
+        "text": "Video Tour"
+      }
+    ]
   },
   "meta": {
     "pageTitlePrefix": "🏡 Stephen Fabbro Open House",
     "navBrandLogoText": "OPEN HOUSE",
-    "privacyPolicyLink": "https://www.mls.theskygroup.ca/privacy-policy"
+    "privacyPolicyLink": "https://property.ca/privacy"
   },
   "settings": {
-   "primaryColor": "",
+    "primaryColor": "",
     "secondaryColor": "",
     "accentColor": "",
     "neutralColor": "",
@@ -70,46 +66,91 @@ window.config = {
     ]
   },
   "modalQuestions": [
-  {
-    "id": "q_auto_1750445840887_0",
-    "questionText": "What's your current situation?",
-    "options": [
-      { "value": "I own my home", "text": "I own my home" },
-      { "value": "I rent", "text": "I rent" },
-      { "value": "Live with family/friends", "text": "Live with family/friends" },
-      { "value": "Between homes", "text": "Between homes" }
-    ]
-  },
-  {
-    "id": "q_auto_1750445840887_1",
-    "questionText": "What are you looking to do?",
-    "options": [
-      { "value": "Buy a home", "text": "Buy a home" },
-      { "value": "Sell and buy", "text": "Sell and buy" },
-      { "value": "Rent a place", "text": "Rent a place" },
-      { "value": "Invest in property", "text": "Invest in property" }
-    ]
-  },
-  {
-    "id": "q_auto_1750445840887_2",
-    "questionText": "When are you planning to move?",
-    "options": [
-      { "value": "ASAP", "text": "ASAP" },
-      { "value": "3-6 months", "text": "3-6 months" },
-      { "value": "6-12 months", "text": "6-12 months" },
-      { "value": "12+ months", "text": "12+ months" }
-    ]
-  },
-  {
-    "id": "agent_status",
-    "questionText": "Are you currently exploring homes with a real estate agent?",
-    "options": [
-      { "value": "committed", "text": "Yes, I'm committed to an agent." },
-      { "value": "not_committed", "text": "No, I'm not committed to an agent." },
-      { "value": "gathering_info", "text": "Just gathering information at this stage." }
-    ]
-  }
-],
+    {
+      "id": "q_auto_1750445840887_0",
+      "questionText": "What's your current situation?",
+      "options": [
+        {
+          "value": "I own my home",
+          "text": "I own my home"
+        },
+        {
+          "value": "I rent",
+          "text": "I rent"
+        },
+        {
+          "value": "Live with family/friends",
+          "text": "Live with family/friends"
+        },
+        {
+          "value": "Between homes",
+          "text": "Between homes"
+        }
+      ]
+    },
+    {
+      "id": "q_auto_1750445840887_1",
+      "questionText": "What are you looking to do?",
+      "options": [
+        {
+          "value": "Buy a home",
+          "text": "Buy a home"
+        },
+        {
+          "value": "Sell and buy",
+          "text": "Sell and buy"
+        },
+        {
+          "value": "Rent a place",
+          "text": "Rent a place"
+        },
+        {
+          "value": "Invest in property",
+          "text": "Invest in property"
+        }
+      ]
+    },
+    {
+      "id": "q_auto_1750445840887_2",
+      "questionText": "When are you planning to move?",
+      "options": [
+        {
+          "value": "ASAP",
+          "text": "ASAP"
+        },
+        {
+          "value": "3-6 months",
+          "text": "3-6 months"
+        },
+        {
+          "value": "6-12 months",
+          "text": "6-12 months"
+        },
+        {
+          "value": "12+ months",
+          "text": "12+ months"
+        }
+      ]
+    },
+    {
+      "id": "agent_status",
+      "questionText": "Are you currently exploring homes with a real estate agent?",
+      "options": [
+        {
+          "value": "committed",
+          "text": "Yes, I'm committed to an agent."
+        },
+        {
+          "value": "not_committed",
+          "text": "No, I'm not committed to an agent."
+        },
+        {
+          "value": "gathering_info",
+          "text": "Just gathering information at this stage."
+        }
+      ]
+    }
+  ],
   "deploymentInfo": {
     "repoId": "1150168931",
     "repoName": "Open-House-685",
